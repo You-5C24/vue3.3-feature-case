6fc9795dc459d224e1425ad782e25e015be7d716
5C24 <youlongte@fhcn.cn>
2023-05-22 15:18:05 +0800

    feat: defineSlots TS类型支持

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,21 @@
   <HelloWorld msg="hello" :age="12" />
   <Generic age="10" :names="['1', '2']" />
   <Emmits />
+  <Sloter>
+    <template v-slot="{ msg }">
+      {{ msg }}
+    </template>
+    <template v-slot:foo="{ bar }">
+      {{ bar }}
+    </template>
+  </Sloter>
 </template>
 
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 import Generic from "./components/Generic.vue";
 import Emmits from "./components/Emmits.vue";
+import Sloter from "./components/Slot.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Slot.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot msg="haha"></slot>
+    <slot name="foo" :bar="1"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineSlots<{
+  default: (props: { msg: string }) => any;
+  foo: (props: { bar: number }) => any;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
