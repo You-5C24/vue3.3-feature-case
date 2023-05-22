af31d464d99263f791e27328726e2875cc2e586d
5C24 <youlongte@fhcn.cn>
2023-05-22 15:00:18 +0800

    feat: generic 泛型支持

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <HelloWorld msg="hello" :age="12" />
+  <Generic age="10" :names="['1', '2']" />
 </template>
 
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import Generic from "./components/Generic.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Generic.vue
@@ -0,0 +1,7 @@
+<template></template>
+
+<script setup lang="ts" generic="T, U extends string">
+defineProps<{ age: T; names: U[] }>();
+</script>
+
+<style lang="scss" scoped></style>
