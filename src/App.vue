9dff7aa6e57ca803ba5570c4b29ae00e7045b5cf
5C24 <youlongte@fhcn.cn>
2023-05-22 15:12:17 +0800

    feat: defineEmit TS新定义方式

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <HelloWorld msg="hello" :age="12" />
   <Generic age="10" :names="['1', '2']" />
+  <Emmits />
 </template>
 
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 import Generic from "./components/Generic.vue";
+import Emmits from "./components/Emmits.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/Emmits.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="handleClickFoo">Foo</button>
+  <button @click="handleClickBar">Bar</button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "foo", id: number): void;
+  (e: "bar", name: string, ...rest: any[]): void;
+}>();
+// const emit = defineEmits<{
+//   foo: [id: number];
+//   bar: [name: string, ...rest: any[]];
+// }>();
+
+const handleClickFoo = () => {
+  emit("foo", 1);
+};
+
+const handleClickBar = () => {
+  emit("bar", "name", 1);
+};
+</script>
+
+<style lang="scss" scoped></style>
